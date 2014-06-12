1b0c016da8529595206eb9c645dac6e9c6f279a6
Ernesto Jiménez <me@ernesto-jimenez.com>
2014-06-12 11:17:25 +0100

    Add ExtractFromResponse method

--- extract.go
+++ extract.go
@@ -2,6 +2,7 @@ package scraperboard
 
 import (
 	"io"
+	"net/http"
 
 	"github.com/mitchellh/mapstructure"
 )
@@ -9,6 +10,16 @@ import (
 // ExtractFromURL scrapes the HTML served in the specified URL into a golang struct
 func (s *Scraper) ExtractFromURL(url string, target interface{}) (err error) {
 	res, err := s.ScrapeFromURL(url)
+	if err != nil {
+		return
+	}
+
+	return mapstructure.Decode(res, target)
+}
+
+// ExtractFromResponse scrapes the HTML served in the specified http response into a golang struct
+func (s *Scraper) ExtractFromResponse(response *http.Response, target interface{}) (err error) {
+	res, err := s.ScrapeFromResponse(response)
 	if err != nil {
 		return
 	}
